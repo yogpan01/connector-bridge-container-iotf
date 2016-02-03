d3722ab6b5433c6d42d19f2d71e7c18a28f43e2f
Doug Anson <doug.anson@arm.com>
2016-02-03 15:11:44 -0600

    updates

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,8 @@ RUN apt-get -y install software-properties-common
 RUN add-apt-repository ppa:webupd8team/java
 RUN apt-get -y update
 RUN apt-get -y install openssh-server supervisor dnsutils
-RUN echo "oracle-java7-installer shared/accepted-oracle-license-v1-1 boolean true" | debconf-set-selections
-RUN apt-get -y --force-yes install oracle-java7-installer
+RUN echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true" | debconf-set-selections
+RUN apt-get -y --force-yes install oracle-java8-installer
 RUN apt-get upgrade
 RUN apt-get -y update
 RUN apt-get -y install unzip zip
@@ -17,9 +17,9 @@ EXPOSE 28520/tcp
 RUN useradd arm -m -s /bin/bash 
 RUN mkdir -p /home/arm
 RUN chown arm.arm /home/arm
-COPY mds-server.zip /home/arm/
+COPY connector-bridge.zip /home/arm/
 COPY configurator-1.0.zip /home/arm/
-RUN chmod 755 /home/arm/mds-server.zip
+RUN chmod 755 /home/arm/connector-bridge.zip
 RUN chmod 755 /home/arm/configurator-1.0.zip
 COPY ssh-keys.tar /home/arm/
 RUN chmod 755 /home/arm/ssh-keys.tar
--- configure_instance.sh
+++ configure_instance.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-setup_mds() 
+setup_bridge() 
 {
     cd /home/arm
-    unzip -q ./mds-server.zip
-    /bin/rm -f ./mds-server.zip
+    unzip -q ./connector-bridge.zip
+    /bin/rm -f ./connector-bridge.zip
     chown -R arm.arm mds *.sh
     chmod -R 700 mds *.sh
 }
@@ -27,15 +27,6 @@ setup_configurator()
    fi
    ln -s ../../mds/connector-bridge/conf/gateway.properties .
    cd ../..
-}
-
-setup_mqtt()
-{
-    IP_ADDRESS=`ifconfig | perl -nle'/dr:(\S+)/ && print $1' | tail -1`
-    mv /home/arm/mds/connector-bridge/conf/gateway.properties .
-    sed "s/192.168.1.34/${IP_ADDRESS}/g" < gateway.properties > /home/arm/mds/connector-bridge/conf/gateway.properties
-    chmod 666 /home/arm/mds/connector-bridge/conf/gateway.properties
-    /bin/rm -f ./gateway.properties
 }
 
 setup_ssh()
@@ -61,20 +52,8 @@ setup_sudoers()
     echo "%arm ALL=NOPASSWD: ALL" >> /etc/sudoers
 }
 
-setup_jce()
-{
-    JRE_HOME=/usr/lib/jvm/java-7-oracle/jre
-    cd ${JRE_HOME}/lib/security
-    unzip /home/arm/jce7.zip
-    mkdir ./backup
-    cp *.jar ./backup
-    cp UnlimitedJCEPolicy/*.jar . 
-    /bin/rm -f /home/arm/jce7.zip
-}
-
 setup_java() {
-    update-alternatives --set java /usr/lib/jvm/java-7-oracle/jre/bin/java
-    # update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
+    update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 }
 
 cleanup()
@@ -84,13 +63,11 @@ cleanup()
 
 main() 
 {
-    setup_mds
+    setup_bridge
     setup_configurator
     setup_ssh
-    # setup_mqtt
     setup_passwords
     setup_sudoers
-    # setup_jce
     setup_java
     cleanup
 }
